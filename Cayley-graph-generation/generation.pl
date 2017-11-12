db83c2c8429d713e4c19506c070c8f34320a822a
seeram <matus.behun@gmail.com>
2017-11-12 21:27:40 +0100

    Second version of algorithm, using array of (dimension of matrix^2 times order of field) size for storing nodes of graph

--- Cayley-graph-generation/generation.pl
+++ Cayley-graph-generation/generation.pl
@@ -11,6 +11,10 @@ use Graph::Directed;
 use Graph::Writer::Dot;
 
 use List::MoreUtils qw(firstidx); 
+use Data::Dumper;
+use Devel::Size qw( size total_size );
+use Number::Format qw( format_bytes );
+use Time::HiRes qw( gettimeofday tv_interval);
 
 sub make_matrix_label
 {
@@ -18,6 +22,13 @@ sub make_matrix_label
 
 	return "((" .index2d($A, 0,0) . ", " . index2d($A, 0,1) . ")," .
 			"(" .index2d($A, 1,0) . ", " . index2d($A, 1,1) . "))";
+}
+
+sub get_index
+{
+	my ($A) = @_;
+
+	return index2d($A, 0,0) . index2d($A, 0,1) . index2d($A, 1,0) . index2d($A, 1,1);
 }
 
 sub print_matrix_arr
@@ -40,7 +51,7 @@ my @generating_set =
 
 @generating_set = map { $_ % $zp } @generating_set;
 my @stack = @generating_set;
-my @nodes = @generating_set;
+my @multiplication_results;
 
 foreach my $generating_element (@generating_set) {
 	if(det($generating_element) == 0) {
@@ -51,23 +62,33 @@ foreach my $generating_element (@generating_set) {
 }
 
 my $current_node = $stack[0]; 
-my $graph = Graph::Undirected->new;
+my @keys = [ get_index($current_node), $current_node ];
 
 while(@stack) {
 	foreach my $generating_element (@generating_set) {
-		my $multiplication_result =  ($current_node x $generating_element) % $zp;
-		my $node_index = firstidx { all $_ == $multiplication_result } @nodes;
-
-		if($node_index == -1) {
-			push @nodes, $multiplication_result;
-			push @stack, $multiplication_result;
-		} 
-
-		$graph->add_edge(make_matrix_label($current_node), make_matrix_label($multiplication_result));
+		my $multiplication_result = $current_node x $generating_element % $zp;	
+		push @{ $multiplication_results[get_index($current_node)] }, $multiplication_result;
+		push @stack, $multiplication_result;
 	}
 
 	shift @stack;
-	$current_node = $stack[0];
+
+	while(@stack && defined($multiplication_results[get_index($stack[0])])) {
+		shift @stack;
+	}
+
+	if(@stack) {
+		$current_node = $stack[0];
+		push @keys, [ get_index($current_node), $current_node ];
+	}
+}
+
+my $graph = Graph::Undirected->new;
+
+foreach my $start_node (@keys) {
+	foreach my $end_node ( @{ $multiplication_results[$start_node->[0]] } )  {
+		$graph->add_edge(make_matrix_label(${start_node}->[1]), make_matrix_label($end_node));
+	}
 }
 
 print "Priemer grafu: " . $graph->diameter(). "\n";
@@ -76,4 +97,4 @@ my $writer = Graph::Writer::Dot->new();
 $writer->write_graph($graph, 'graf.dot');
 
 system "dot", "-Tps", "graf.dot", "-o", "outfile.ps";
-system "circo", "-Tsvg", "graf.dot", "-o", "outfile.svg";
+# system "circo", "-Tsvg", "graf.dot", "-o", "outfile.svg";
